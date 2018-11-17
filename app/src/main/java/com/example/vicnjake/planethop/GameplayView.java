ee06329136302f6b350d22bda99acadc15d6eb08
Jake B <jakebeinart@gmail.com>
2018-11-17 14:05:54 -0600

    Pilot now circles the home planet on start

--- app/src/main/java/com/example/vicnjake/planethop/GameplayView.java
+++ app/src/main/java/com/example/vicnjake/planethop/GameplayView.java
@@ -34,13 +34,13 @@ public class GameplayView extends SurfaceView implements Runnable{
         this.screenSizeY = screenSizeY;
         paint = new Paint();
 
-        pilotx=screenSizeX/2+125;
+        pilotx=screenSizeX/2;
         piloty=(screenSizeY*9/10);
         angle=0;
 
         homePlanet = new DrawableObject(context, R.drawable.planet1, new int[]{screenSizeX/2, (screenSizeY*9/10)}, new int[]{200, 200});
         gravity = new DrawableObject(context, R.drawable.gravity, new int[]{screenSizeX/2, (screenSizeY*9/10)}, new int[]{260, 260});
-        pilot = new DrawableObject(context, R.drawable.pilot_ship, new int[]{screenSizeX/2+125, (screenSizeY*9/10)}, new int[]{50, 50});
+        pilot = new DrawableObject(context, R.drawable.pilot_ship, new int[]{screenSizeX/2, (screenSizeY*9/10)}, new int[]{50, 50});
 
         setFocusable(true);
 
@@ -70,8 +70,8 @@ public class GameplayView extends SurfaceView implements Runnable{
         if (surfaceHolder.getSurface().isValid()) {
             canvas = surfaceHolder.lockCanvas();
             
-            angle+=1;
-            Log.i("DEBUGGIN!", String.valueOf(Math.cos(Math.toRadians(angle%360))*125));
+
+            Log.i("DEBUGGIN!", String.valueOf((int)(Math.cos(Math.toRadians(angle%360))*130)));
             canvas.drawBitmap(
                 homePlanet.getBitmap(),
                     homePlanet.getCoords()[0],
@@ -88,11 +88,14 @@ public class GameplayView extends SurfaceView implements Runnable{
             );
             canvas.drawBitmap(
                     pilot.getBitmap(),
-                    pilotx + (int)(Math.cos(Math.toRadians(angle%360))*125),
-                    piloty + (int)(Math.sin(Math.toRadians(angle%360))*125),
+                    pilot.getCoords()[0]+(int)(Math.cos(Math.toRadians(angle))*130),
+                    pilot.getCoords()[1]+(int)(Math.sin(Math.toRadians(angle))*130),
+                    //pilotx + (int)(Math.cos(Math.toRadians(angle))*130),
+                    //piloty + (int)(Math.sin(Math.toRadians(angle))*130),
                     paint
 
             );
+            angle=(angle+2)%360;
             surfaceHolder.unlockCanvasAndPost(canvas);
         }
     }
