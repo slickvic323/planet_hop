6e874fbb3ad3284332eb3fe4edf6de239f7d4de5
Victor <vicdasilva32@gmail.com>
2018-11-17 10:22:06 -0600

    Created Android Studio Project

---
+++ app/src/main/java/com/example/vicnjake/planethop/GameplayActivity.java
@@ -0,0 +1,13 @@
+package com.example.vicnjake.planethop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameplay);
+    }
+}
